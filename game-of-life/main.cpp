267686753216aa6d630bd137d7056e528c9b56db
Sami Hatna <sami.hatna2003@gmail.com>
2022-01-07 16:31:21 +0000

    Initial Commit

---
+++ game-of-life/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  game-of-life
+//
+//  Created by Sami Hatna on 07/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
